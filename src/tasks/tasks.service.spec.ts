6958abbd468d80e497576ef52cb46c1c2d19b7ed
Pedro Alfonso Garcia <p.alfonsoag@gmail.com>
2024-08-15 11:48:53 -0400

    adding some test complex test cases to use them as reference

--- src/tasks/tasks.service.spec.ts
+++ src/tasks/tasks.service.spec.ts
@@ -4,10 +4,11 @@ import { TaskRepository } from './task.repository';
 import { User } from '../user/user.entity';
 import { Task } from './task.entity';
 import { UserService } from '../user/user.service';
+import { Priority, TasksStatus } from './tasks-enums';
 
 const mockUser: User = {
   id: 'myTestId',
-  username: 'ammar_alkhooly',
+  username: 'random_user',
   email: 'email@email.com',
   password: 'testPassword',
   name: 'user',
@@ -15,26 +16,49 @@ const mockUser: User = {
   tasksCreated: [],
 };
 
-const tasks: Task[] = [];
+const mockTaskResults: Task[] = [
+  {
+    id: 'mockId1',
+    title: 'mock Title 1',
+    description: '',
+    status: TasksStatus.IN_PROGRESS,
+    priority: Priority.P1,
+    hide: false,
+    createdAt: new Date('10/10/2024'),
+    createdBy: mockUser,
+    dueDate: new Date('10/11/2024'),
+  },
+  {
+    id: 'mockId2',
+    title: 'mock Title 2',
+    description: '',
+    status: TasksStatus.IN_PROGRESS,
+    priority: Priority.P1,
+    hide: false,
+    createdAt: new Date('10/10/2024'),
+    createdBy: mockUser,
+    dueDate: new Date('10/11/2024'),
+  },
+];
 
 export const mockTaskRepository = () => ({
-  getTasks: jest.fn(),
+  getTasks: jest.fn().mockResolvedValue(mockTaskResults),
   createQueryBuilder: jest.fn(() => ({
     where: jest.fn().mockReturnThis(),
     andWhere: jest.fn().mockReturnThis(),
-    getMany: jest.fn().mockReturnThis().mockResolvedValue(tasks),
+    getMany: jest.fn().mockReturnThis().mockResolvedValue(mockTaskResults),
   })),
   create: jest.fn(),
   delete: jest.fn(),
   save: jest.fn(),
-  findOne: jest.fn(),
+  findOneBy: jest.fn().mockResolvedValue(mockTaskResults[0]),
 });
 
 const mockUserService = () => ({ findUserById: jest.fn() });
 
 describe('TasksService', () => {
   let service: TasksService;
-  let repository: TaskRepository;
+  let repository: jest.Mocked<TaskRepository>;
   let userService: UserService;
 
   beforeEach(async () => {
@@ -47,11 +71,35 @@ describe('TasksService', () => {
     }).compile();
 
     service = module.get<TasksService>(TasksService);
-    repository = module.get<TaskRepository>(TaskRepository);
+    repository = module.get<jest.Mocked<TaskRepository>>(TaskRepository);
     userService = module.get<UserService>(UserService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('getTasks', () => {
+    it('should call TaskRepository.getTasks return all tasks', async () => {
+      const result = await service.getTasks({}, mockUser);
+
+      expect(result).toEqual(mockTaskResults);
+    });
+  });
+
+  describe('getTaskById', () => {
+    it('should call TaskRepository.findOneBy return a task', async () => {
+      const result = await service.getTaskById(mockTaskResults[0].id);
+
+      expect(result).toEqual(mockTaskResults[0]);
+    });
+
+    it('should call TaskRepository.findOneBy and handle the error', async () => {
+      repository.findOneBy.mockResolvedValue(null);
+
+      expect(service.getTaskById(mockTaskResults[0].id)).rejects.toThrow(
+        `Task with ID "${mockTaskResults[0].id}" not found`,
+      );
+    });
   });
 });
